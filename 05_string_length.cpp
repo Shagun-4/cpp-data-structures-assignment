21cf2427b3fc15394e260c13f387f8c9f22149b0
Shagun <shagun.lpu22@gmail.com>
2026-08-03 21:57:49 +0530

    Add string length counting program

---
+++ 05_string_length.cpp
@@ -0,0 +1,13 @@
+//Strings - Count number of characters in a string
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+
+    cout << str.length();
+
+    return 0;
+}
