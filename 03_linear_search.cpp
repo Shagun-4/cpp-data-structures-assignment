31fa4adfc0ff2673555fcc9749502ab924da534f
Shagun <shagun.lpu22@gmail.com>
2026-08-03 21:55:39 +0530

    Implement linear search algorithm

---
+++ 03_linear_search.cpp
@@ -0,0 +1,31 @@
+//Searching - Check if a number exists in an array
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int target;
+    cin >> target;
+
+    bool found = false;
+
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == target) {
+            found = true;
+            break;
+        }
+    }
+
+    if(found)
+        cout << "Found";
+    else
+        cout << "Not Found";
+
+    return 0;
+}
