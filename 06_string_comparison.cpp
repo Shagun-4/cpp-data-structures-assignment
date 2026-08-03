bb5f6d19023eea74bb8353e383161643dda04135
Shagun <shagun.lpu22@gmail.com>
2026-08-03 21:58:58 +0530

    Implement string comparison for equality check

---
+++ 06_string_comparison.cpp
@@ -0,0 +1,16 @@
+//Strings - Check if two strings are equal
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    if(s1 == s2)
+        cout << "Equal";
+    else
+        cout << "Not Equal";
+
+    return 0;
+}
